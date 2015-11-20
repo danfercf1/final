c3cea601e9ae243fda6c70550d33341ecea8e4b9
JoannaLeslye <joannaleslye@gmail.com>
2015-11-20 16:41:37 -0400

    modificacion r-general

--- views/site/r_general.php
+++ views/site/r_general.php
@@ -4,12 +4,14 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\bootstrap\Tabs;
 
+use kartik\widgets\Select2;
+
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\EstudiantesBusqueda */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Ranking general';
-$this->params['breadcrumbs'][] = ['label' => 'clasificacion', 'url' => ['clasificados']];
+$this->params['breadcrumbs'][] = ['label' => 'Clasificacion', 'url' => ['clasificados']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="clasificacion">
@@ -21,7 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 //var_dump($urbano);
 
-$test1 = GridView::widget([
+$distrito = GridView::widget([
                             'dataProvider' => $dataProvider,
                             'filterModel' => $searchModel,
                             'columns' => [
@@ -29,105 +31,214 @@ $test1 = GridView::widget([
                     
                                 //'_id',
                                 'DISTRITO',
-                                //'CURSO',
                                 'PATERNO',
                                 'MATERNO',
                                 'NOMBRE',
                                 'NOTA',      
-                            ],  
-                            
+                            ],     
                         ]);
+                 
                         
-$test2 = GridView::widget([
-                            'dataProvider' => $dataProvider,
+$curso1 = GridView::widget([
+                            'dataProvider' => $dataProvider1S,
                             'filterModel' => $searchModel,
                             'columns' => [
                                 ['class' => 'yii\grid\SerialColumn'],
                     
                                 //'_id',
                                 'CURSO',
-                                //'CURSO',
                                 'PATERNO',
                                 'MATERNO',
                                 'NOMBRE',
                                 'NOTA',
-                            ],  
-                            
-                        ]);
-                       
-                       
-$test3a = GridView::widget([
-                            'dataProvider' => $dataProvider,
-                            'filterModel' => $searchModel,
-                            'columns' => [
-                                ['class' => 'yii\grid\SerialColumn'],                    
-                                
-                                //'_id',
-                                ['label'=>'Area Rural', 'format'=>'raw','value'=>function($model){return $model->uE->AREA;}],
-                                //'CURSO',
-                                'PATERNO',
-                                'MATERNO',
-                                'NOMBRE',
-                                'NOTA',
-                                //'UNIDAD_EDUCATIVA',  
-                            ],  
-                            
+                            ],   
                         ]);
                         
-$test3b = GridView::widget([
-                            'dataProvider' => $dataProvider,
-                            'filterModel' => $searchModel,
-                            'columns' => [
-                                ['class' => 'yii\grid\SerialColumn'],                    
-                                
-                                //'_id',
-                                ['label'=>'Area Urbana', 'format'=>'raw','value'=>function($model){return $model->uE->AREA;}],
-                                //'CURSO',
-                                'PATERNO',
-                                'MATERNO',
-                                'NOMBRE',
-                                'NOTA',
-                                //'UNIDAD_EDUCATIVA', 
-                            ],  
-                            
-                        ]);
-                    
-$test4 = GridView::widget([
-                            'dataProvider' => $dataProvider,
+$curso2 = GridView::widget([
+                            'dataProvider' => $dataProvider2S,
                             'filterModel' => $searchModel,
                             'columns' => [
                                 ['class' => 'yii\grid\SerialColumn'],
                     
                                 //'_id',
-                                //'DEPENDENCIA',
-                                ['label'=>'DEPENDENCIA', 'format'=>'raw','value'=>function($model){return $model->uE->DEPENDENCIA;}],
-                                //'CURSO',
+                                'CURSO',
                                 'PATERNO',
                                 'MATERNO',
                                 'NOMBRE',
                                 'NOTA',
-                            ],  
-                            
+                            ],   
                         ]);
 
-$test5 = GridView::widget([
-                            'dataProvider' => $dataProvider,
+$curso3 = GridView::widget([
+                            'dataProvider' => $dataProvider3S,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'CURSO',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],   
+                        ]);
+
+$curso4 = GridView::widget([
+                            'dataProvider' => $dataProvider4S,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'CURSO',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],   
+                        ]);
+                        
+$curso5 = GridView::widget([
+                            'dataProvider' => $dataProvider5S,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'CURSO',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],   
+                        ]);
+
+$curso6 = GridView::widget([
+                            'dataProvider' => $dataProvider6S,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'CURSO',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],   
+                        ]);
+                        
+                       
+$rural = GridView::widget([
+                            'dataProvider' => $dataProviderR,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],                    
+                                
+                                //'_id',
+                                'AREA',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],   
+                        ]);
+                                     
+$urbano = GridView::widget([
+                            'dataProvider' => $dataProviderU,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],                    
+                                
+                                //'_id',
+                                'AREA',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],    
+                        ]);
+                    
+                    
+$convenio = GridView::widget([
+                            'dataProvider' => $dataProviderC,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'DEPENDENCIA',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],      
+                        ]);
+
+$fiscal = GridView::widget([
+                            'dataProvider' => $dataProviderF,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'DEPENDENCIA',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],    
+                        ]);
+                        
+$privado = GridView::widget([
+                            'dataProvider' => $dataProviderP,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'DEPENDENCIA',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],     
+                        ]);
+
+
+$femenino = GridView::widget([
+                            'dataProvider' => $dataProviderGF,
                             'filterModel' => $searchModel,
                             'columns' => [
                                 ['class' => 'yii\grid\SerialColumn'],
                     
                                 //'_id',
                                 'GENERO',
-                                //'CURSO',
+                                'PATERNO',
+                                'MATERNO',
+                                'NOMBRE',
+                                'NOTA',
+                            ],    
+                        ]);
+
+$masculino = GridView::widget([
+                            'dataProvider' => $dataProviderGM,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                    
+                                //'_id',
+                                'GENERO',
                                 'PATERNO',
                                 'MATERNO',
                                 'NOMBRE',
                                 'NOTA',
                             ],  
-                            
                         ]);
 
-$test6 = GridView::widget([
+
+$sub15 = GridView::widget([
                             'dataProvider' => $dataProvider,
                             'filterModel' => $searchModel,
                             'columns' => [
@@ -135,16 +246,14 @@ $test6 = GridView::widget([
                     
                                 //'_id',
                                 'EDAD',
-                                //'CURSO',
                                 'PATERNO',
                                 'MATERNO',
                                 'NOMBRE',
                                 'NOTA',
-                            ],  
-                            
+                            ],    
                         ]);
 
-$test7 = GridView::widget([
+$sub17 = GridView::widget([
                             'dataProvider' => $dataProvider,
                             'filterModel' => $searchModel,
                             'columns' => [
@@ -152,13 +261,11 @@ $test7 = GridView::widget([
                     
                                 //'_id',
                                 'EDAD',
-                                //'CURSO',
                                 'PATERNO',
                                 'MATERNO',
                                 'NOMBRE',
                                 'NOTA',
-                            ],  
-                            
+                            ],   
                         ]);
                         
                         
@@ -167,12 +274,51 @@ echo Tabs::widget([
     'items' => [
         [
             'label' => 'Distrito',
-            'content' => '<div>'.$test1.'</div>',
+            'content' => '
+                        <div class="selector">
+                        <label>Seleccionar distrito</label>
+                        <select> 
+                            <option>distrito 1</option>
+                            <option>distrito 2</option>
+                            <option>distrito 3</option>
+                        </select>
+                        <button>cargar</button>
+                        </div>
+                        
+                        <div>'.$distrito.'</div>
+                        ',
+           
             'active' => true
         ],
         [
-         'label' => 'Curso',
-         'content' => '<div>'.$test2.'</div>',  
+         'label' => 'Curso',                
+         //'content' => '<div>'.$curso.'</div>',  
+         'items' => [
+                    [
+                        'label' => '1ro Sec.',
+                        'content' => '<div>'.$curso1.'</div>',
+                    ],
+                    [
+                        'label' => '2do Sec.',
+                        'content' => '<div>'.$curso2.'</div>',
+                    ],
+                    [
+                        'label' => '3ro Sec.',
+                        'content' => '<div>'.$curso3.'</div>',
+                    ],
+                    [
+                        'label' => '4to Sec.',
+                        'content' => '<div>'.$curso4.'</div>',
+                    ],
+                    [
+                        'label' => '5to Sec.',
+                        'content' => '<div>'.$curso5.'</div>',
+                    ],
+                    [
+                        'label' => '6to Sec.',
+                        'content' => '<div>'.$curso6.'</div>',
+                    ],
+               ],
         ],
         [
          'label' => 'Area Regional',
@@ -180,29 +326,47 @@ echo Tabs::widget([
          'items' => [
                     [
                         'label' => 'Rural',
-                        'content' => '<div>'.$test3a.'</div>',
+                        'content' => '<div>'.$rural.'</div>',
                     ],
                     [
                         'label' => 'Urbana',
-                        'content' => '<div>'.$test3b.'</div>',
+                        'content' => '<div>'.$urbano.'</div>',
                     ],
                ],
         ],
         [
          'label' => 'Dependencia',
-         'content' => '<div>'.$test4.'</div>',  
+         //'content' => '<div>'.$test4.'</div>',  
+         'items' => [
+                    [
+                        'label' => 'Convenio',
+                        'content' => '<div>'.$convenio.'</div>',
+                    ],
+                    [
+                        'label' => 'Fiscal o estatal',
+                        'content' => '<div>'.$fiscal.'</div>',
+                    ],
+                    [
+                        'label' => 'Privada',
+                        'content' => '<div>'.$privado.'</div>',
+                    ],
+               ],
         ],
         [
-         'label' => 'Genero',
-         'content' => '<div>'.$test5.'</div>',  
+         'label' => 'Femenino',
+         'content' => '<div>'.$femenino.'</div>',  
+        ],
+        [
+         'label' => 'Masculino',
+         'content' => '<div>'.$masculino.'</div>',  
         ],
         [
          'label' => 'Sub-15',
-         'content' => '<div>'.$test6.'</div>',  
+         'content' => '<div>'.$sub15.'</div>',  
         ],
         [
          'label' => 'Sub-17',
-         'content' => '<div>'.$test7.'</div>',  
+         'content' => '<div>'.$sub17.'</div>',  
         ]
     ],
     

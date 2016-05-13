784c58c94d5c5c93419ed1b88baa5feeb20481e7
danfercf1 <danfercf@gmail.com>
2016-05-13 14:51:57 -0400

    Mongo config

--- web/listas_excel/instaladores/Configurar Mongo/1. Configuracion Mongo.bat
+++ web/listas_excel/instaladores/Configurar Mongo/1. Configuracion Mongo.bat
@@ -5,8 +5,6 @@ set /p unidad="Ingrese la unidad donde instalo Ampps: "
 
 START "" /wait /b %unidad%":\Ampps\mongodb\bin\mongo.exe" admin "config.js"
 
-START "" /wait /b %unidad%":\Ampps\mongodb\bin\mongo.exe" datos "add.js"
-
 :: SLEEP 3
 
 :: END
---
+++ web/listas_excel/instaladores/Configurar Mongo/2. Configuracion Mongo.bat
@@ -0,0 +1,10 @@
+@echo off
+:: BEGIN
+
+set /p unidad="Ingrese la unidad donde instalo Ampps: "
+
+START "" /wait /b %unidad%":\Ampps\mongodb\bin\mongo.exe" datos "add.js"
+
+:: SLEEP 3
+
+:: END
